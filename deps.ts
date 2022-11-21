a86ba3cd60542164379606a490e070f7b8001a50
CGQAQ <m.jason.liu@outlook.com>
2022-11-21 17:30:21 +0800

    backup

--- deps.ts
+++ deps.ts
@@ -1,1 +1,3 @@
 export * as Dom from "https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts";
+
+export { Octokit as Github } from "npm:@octokit/rest@19.0.5";
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 console.log("hello world from deno");
 
 // ranking url
-import { Dom } from "./deps.ts";
+import { Dom, Github } from "./deps.ts";
 const { DOMParser } = Dom;
 
 const URL = "https://www.biquge.co/paihangbang/";
@@ -10,3 +10,36 @@ const gbkTextDecoder = new TextDecoder("gbk");
 const resp = await fetch(URL);
 const content = gbkTextDecoder.decode(await resp.arrayBuffer());
 const doc = new DOMParser().parseFromString(content, "text/html");
+// #main > .box
+const container = doc?.querySelectorAll("#main > .box");
+
+if (!container) {
+  throw new Error("container is null");
+}
+
+const result = [...container].map((category) => {
+  const element = category as unknown as Dom.Element;
+  const title = element.querySelector("h3")?.textContent;
+
+  const list = [...element.querySelectorAll("li > a")].map((node) => {
+    const element = node as unknown as Dom.Element;
+    return {
+      href: element.getAttribute("href"),
+      title: element.textContent,
+    };
+  });
+
+  return {
+    title,
+    list,
+  };
+});
+
+const github = new Github();
+const myProjs = (
+  await github.projects.listForUser({
+    username: "cgqaq",
+  })
+).data;
+
+console.log(myProjs);
