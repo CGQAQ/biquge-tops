1d24b95c305d9213e3d79da429263c0872d5238a
CGQAQ <m.jason.liu@outlook.com>
2022-11-21 16:50:48 +0800

    add deno dom

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
 console.log("hello world from deno");
 
 // ranking url
+import { Dom } from "./deps.ts";
+const { DOMParser } = Dom;
+
 const URL = "https://www.biquge.co/paihangbang/";
+const gbkTextDecoder = new TextDecoder("gbk");
+
+const resp = await fetch(URL);
+const content = gbkTextDecoder.decode(await resp.arrayBuffer());
+const doc = new DOMParser().parseFromString(content, "text/html");
