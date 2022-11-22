f04e4ecd8f38e5e377ed615c31c200ccbaf2a177
CGQAQ <m.jason.liu@outlook.com>
2022-11-22 12:48:20 +0800

    text encode

--- main.ts
+++ main.ts
@@ -51,7 +51,7 @@ const x = await github.rest.repos.createOrUpdateFileContents({
   repo: Deno.env.get("GITHUB_REPOSITORY")?.split("/")?.[1] || "",
   path: `rankings/ranking-${time}.json`,
   message: `update ranking ${time}`,
-  content: b64Encode(JSON.stringify(result)),
+  content: b64Encode(new TextEncoder().encode(JSON.stringify(result))),
 });
 console.log("after");
 
