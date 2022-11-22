62f4a2c6ea6e61d6c00ee8dd1c1687df8f59e400
CGQAQ <m.jason.liu@outlook.com>
2022-11-22 12:38:48 +0800

    fix  path cannot start with a slash

--- main.ts
+++ main.ts
@@ -49,7 +49,7 @@ const time = format(new Date(), "yyyy-MM-dd HH:mm:ss");
 const x = await github.rest.repos.createOrUpdateFileContents({
   owner: Deno.env.get("GITHUB_ACTOR") || "",
   repo: Deno.env.get("GITHUB_REPOSITORY")?.split("/")?.[1] || "",
-  path: `/rankings/ranking-${time}.json`,
+  path: `rankings/ranking-${time}.json`,
   message: `update ranking ${time}`,
   content: JSON.stringify(result),
 });
